f54c21046948b65095aea09e5556ba03dd783515
anmatika <4ntti.matikainen@gmail.com>
2021-04-12 14:44:40 +0300

    Add run animations

--- src/index.js
+++ src/index.js
@@ -8,13 +8,19 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('tileset', 'src/assets/tileset.png');
-    this.load.image('player', 'src/assets/player.png');
-    this.load.tilemapTiledJSON('map', 'src/assets/town.json');
+    this.load.image('tileset', 'assets/tileset.png');
+    // this.load.image('player', 'assets/player.png');
+    this.load.tilemapTiledJSON('map', 'assets/town.json');
+
+    this.load.multiatlas('atlas', 'assets/spritesheets/player/player.json', 'assets');
+    // this.load.spritesheet('player', 'assets/spritesheets/player/spritesheet/player.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('player', 'assets/spritesheets/player.png', { frameWidth: 64, frameHeight: 64 });
+    // this.load.spritesheet('player', 'assets/spritesheets/mummy37x45.png', { frameWidth: 37, frameHeight: 45 });
   }
 
   create() {
     this.setupWorld();
+    this.setupAnims();
     this.setupPlayer();
     this.setupPhysics();
     this.setupCamera();
@@ -26,6 +32,29 @@ class MyGame extends Phaser.Scene {
   update() {
     // NOTE Evernote webclipper must be set off. Breaks the game.
     this.handleMovementWithVelocity();
+    this.handleAnims();
+  }
+
+  setupAnims() {
+    const { anims } = this;
+    anims.create({
+      key: 'runRight',
+      frameRate: 10,
+      repeat: -1,
+      frames: this.anims.generateFrameNumbers('player', { start: 5, end: 9 }),
+    });
+    anims.create({
+      key: 'runUp',
+      frameRate: 10,
+      repeat: -1,
+      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 4 }),
+    });
+    anims.create({
+      key: 'runDown',
+      frameRate: 10,
+      repeat: -1,
+      frames: this.anims.generateFrameNumbers('player', { start: 20, end: 24 }),
+    });
   }
 
   setupWorld() {
@@ -49,6 +78,8 @@ class MyGame extends Phaser.Scene {
     this.player = this.physics.add.sprite(50, 100, 'player')
       .setCollideWorldBounds(true)
       .setDepth(2);
+
+    console.log('player', this.player);
   }
 
   setupPhysics() {
@@ -74,6 +105,26 @@ class MyGame extends Phaser.Scene {
       this.player.body.setVelocityY(-speed);
     } else if (this.cursors.down.isDown) {
       this.player.body.setVelocityY(speed);
+    }
+  }
+
+  handleAnims() {
+    if (this.cursors.left.isDown) {
+      // this.player.anims.play('misa-left-walk', true);
+      this.player.anims.play('runRight', true);
+      this.player.setFlipX(true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setFlipX(false);
+      this.player.anims.play('runRight', true);
+    } else if (this.cursors.up.isDown) {
+      this.player.anims.play('runUp', true);
+      this.player.setFlipX(false);
+    } else if (this.cursors.down.isDown) {
+      this.player.anims.play('runDown', true);
+      this.player.setFlipX(false);
+      // this.player.anims.play('misa-front-walk', true);
+    } else {
+      this.player.anims.stop();
     }
   }
 }
