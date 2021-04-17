58f1762f722f3c4794ac66004c55daba7fd7c662
Antti Matikainen <antti.matikainen@sievo.com>
2021-04-17 09:44:32 +0300

    Renaming

--- src/World/index.js
+++ src/World/index.js
@@ -9,10 +9,10 @@ class World {
   setupWorld() {
     this.map = this.scene.make.tilemap({ key: 'map' });
     this.tilesetGrass = this.map.addTilesetImage('grasstiles');
-    this.tilesetWorld = this.map.addTilesetImage('houses');
+    this.tilesetHouses = this.map.addTilesetImage('houses');
     this.backgroundLayer = this.map.createLayer('BaseLayer', this.tilesetGrass, 0, 0).setScale(1).setDepth(1);
-    this.worldLayer = this.map.createLayer('WorldLayer', this.tilesetWorld, 0, 0).setScale(1).setDepth(2);
-    this.worldLayer.setCollisionByExclusion([-1]);
+    this.collideLayer = this.map.createLayer('CollideLayer', this.tilesetHouses, 0, 0).setScale(1).setDepth(2);
+    this.collideLayer.setCollisionByExclusion([-1]);
   }
 
   static Create(scene) {
