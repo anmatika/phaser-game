d9230b51e53a31134d99bc48bc3ba7c2fd9224e8
anmatika <4ntti.matikainen@gmail.com>
2021-04-23 11:11:44 +0300

    Set wasd

--- src/Input/index.ts
+++ src/Input/index.ts
@@ -1,14 +1,17 @@
 import 'phaser';
-type Keys = {
+class Keys {
   W: Phaser.Input.Keyboard.Key
   A: Phaser.Input.Keyboard.Key
   S: Phaser.Input.Keyboard.Key
   D: Phaser.Input.Keyboard.Key
-}
 
-class Keyboard {
+  constructor(scene) {
+    this.A = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.S = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    this.D = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.W = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  }
 }
-
 class Input {
   private scene!: Phaser.Scene
   public cursors!: Phaser.Types.Input.Keyboard.CursorKeys
@@ -18,33 +21,25 @@ class Input {
     scene,
   }) {
     this.scene = scene;
-    this.setupKeys();
+    this.keys = new Keys(scene)
+    this.cursors = this.scene.input.keyboard.createCursorKeys();
   }
 
   public get isLeftDown(): boolean {
     return this.cursors.left.isDown
-    // || this.keys.A.isDown
+      || this.keys.A.isDown
   }
   public get isRightDown(): boolean {
     return this.cursors.right.isDown
-    // || this.keys.D.isDown
+      || this.keys.D.isDown
   }
   public get isDownDown(): boolean {
     return this.cursors.down.isDown
-    //  || this.keys.S.isDown
+      || this.keys.S.isDown
   }
   public get isUpDown(): boolean {
     return this.cursors.up.isDown
-    // || this.keys.W.isDown
-  }
-
-  private setupKeys() {
-    this.cursors = this.scene.input.keyboard.createCursorKeys();
-
-    // this.keys.A = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    // this.keys.S = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    // this.keys.D = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    // this.keys.W = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+      || this.keys.W.isDown
   }
 }
 
