84e4ab230f43e8fc7ea70bd085b7db5c8f9ed2dc
Antti Matikainen <antti.matikainen@sievo.com>
2021-11-30 06:50:16 +0200

    Add backend eslint

---
+++ Backend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': ['off'],
+    'no-useless-constructor': ['off'],
+    'no-console': ['off'],
+    'no-new': ['off'],
+    'no-unused-vars': ['warn'],
+    'max-len': ['off'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+
+  },
+};
